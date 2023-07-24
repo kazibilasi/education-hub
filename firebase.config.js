aac2ee4665586f0677379aa81e5cd7ab4f27a234
kazibilsi <kazibilasi@gmail.com>
2023-07-24 14:52:05 +0600

    Firebase Initial Deploy

--- firebase.config.js
+++ firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBwYwqdK9uj_t_HAu7MsONxbn4s4nNH6pI",
-  authDomain: "education-hub-c771f.firebaseapp.com",
-  projectId: "education-hub-c771f",
-  storageBucket: "education-hub-c771f.appspot.com",
-  messagingSenderId: "342284326994",
-  appId: "1:342284326994:web:2ac4681e26fa9fe816278b"
+  apiKey: "AIzaSyAPIfB821yXF-N5IwXTf1cxaPQFDvghKRo",
+  authDomain: "education-hub-fc3dc.firebaseapp.com",
+  projectId: "education-hub-fc3dc",
+  storageBucket: "education-hub-fc3dc.appspot.com",
+  messagingSenderId: "966707588307",
+  appId: "1:966707588307:web:83b14d0ee74de5c9cab826"
 };
 
 // Initialize Firebase
